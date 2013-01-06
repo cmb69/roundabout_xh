92dada145d2a3c2703a7bfa583b313ca7b952c7d
cmb69 <cmbecker69@gmx.de>
2013-01-06 13:38:38 +0000

    formatted according to XH CS

--- admin.php
+++ admin.php
@@ -1,12 +1,14 @@
 <?php
 
 /**
- * Back-End of Roundabout_XH.
+ * Back-end of Roundabout_XH.
  *
- * Copyright (c) 2012 Christoph M. Becker (see license.txt)
+ * @package	Roundabout
+ * @copyright	Copyright (c) 2012-2013 Christoph M. Becker <http://3-magi.net/>
+ * @license	http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version     $Id$
+ * @link	http://3-magi.net/?CMSimple_XH/Roundabout_XH
  */
-
-// utf8-marker: äöüß
 
 
 if (!defined('CMSIMPLE_XH_VERSION')) {
@@ -20,76 +22,81 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
  *
  * @return string  The (X)HTML.
  */
-function roundabout_version() {
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Roundabout_XH">Roundabout_XH</a></h1>'."\n"
-	    .'<p>Version: '.ROUNDABOUT_VERSION.'</p>'."\n"
-	    .'<p>Copyright &copy; 2012 <a href="http://3-magi.net">Christoph M. Becker</a></p>'."\n"
-	    .'<p>Roundabout_XH is powered by <a href="http://www.jcarousel.de/">jQuery Carousel</a>.</p>'."\n"
-	    .'<p style="text-align: justify">This program is free software: you can redistribute it and/or modify'
-	    .' it under the terms of the GNU General Public License as published by'
-	    .' the Free Software Foundation, either version 3 of the License, or'
-	    .' (at your option) any later version.</p>'."\n"
-	    .'<p style="text-align: justify">This program is distributed in the hope that it will be useful,'
-	    .' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	    .' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	    .' GNU General Public License for more details.</p>'."\n"
-	    .'<p style="text-align: justify">You should have received a copy of the GNU General Public License'
-	    .' along with this program.  If not, see'
-	    .' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>'."\n";
+function Roundabout_version()
+{
+    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Roundabout_XH">Roundabout_XH</a></h1>'
+	. '<p>Version: ' . ROUNDABOUT_VERSION . '</p>'
+	. '<p>Copyright &copy; 2012 <a href="http://3-magi.net">Christoph M. Becker</a></p>'
+	. '<p>Roundabout_XH is powered by <a href="http://www.jcarousel.de/">jQuery Carousel</a>.</p>'
+	. '<p style="text-align: justify">This program is free software: you can redistribute it and/or modify'
+	. ' it under the terms of the GNU General Public License as published by'
+	. ' the Free Software Foundation, either version 3 of the License, or'
+	. ' (at your option) any later version.</p>'
+	. '<p style="text-align: justify">This program is distributed in the hope that it will be useful,'
+	. ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
+	. ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
+	. ' GNU General Public License for more details.</p>'
+	. '<p style="text-align: justify">You should have received a copy of the GNU General Public License'
+	. ' along with this program.  If not, see'
+	. ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>';
 }
 
 
 /**
  * Returns the requirements information view.
  *
+ * @global array
+ * @global array
+ * @global array
  * @return string  The (X)HTML.
  */
-function roundabout_system_check() { // RELEASE-TODO
+function Roundabout_systemCheck()
+{ // RELEASE-TODO
     global $pth, $tx, $plugin_tx;
 
     define('ROUNDABOUT_PHP_VERSION', '4.0.7');
-    $ptx =& $plugin_tx['roundabout'];
-    $imgdir = $pth['folder']['plugins'].'roundabout/images/';
-    $ok = tag('img src="'.$imgdir.'ok.png" alt="ok"');
-    $warn = tag('img src="'.$imgdir.'warn.png" alt="warning"');
-    $fail = tag('img src="'.$imgdir.'fail.png" alt="failure"');
-    $htm = tag('hr').'<h4>'.$ptx['syscheck_title'].'</h4>'
-	    .(version_compare(PHP_VERSION, ROUNDABOUT_PHP_VERSION) >= 0 ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.sprintf($ptx['syscheck_phpversion'], ROUNDABOUT_PHP_VERSION)
-	    .tag('br').tag('br')."\n";
+    $ptx = $plugin_tx['roundabout'];
+    $imgdir = $pth['folder']['plugins'] . 'roundabout/images/';
+    $ok = tag('img src="' . $imgdir . 'ok.png" alt="ok"');
+    $warn = tag('img src="' . $imgdir . 'warn.png" alt="warning"');
+    $fail = tag('img src="' . $imgdir . 'fail.png" alt="failure"');
+    $o = tag('hr') . '<h4>' . $ptx['syscheck_title'] . '</h4>'
+	. (version_compare(PHP_VERSION, ROUNDABOUT_PHP_VERSION) >= 0 ? $ok : $fail)
+	. '&nbsp;&nbsp;' . sprintf($ptx['syscheck_phpversion'], ROUNDABOUT_PHP_VERSION)
+	. tag('br') . tag('br');
     foreach (array() as $ext) {
-	$htm .= (extension_loaded($ext) ? $ok : $fail)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_extension'], $ext).tag('br')."\n";
+	$o .= (extension_loaded($ext) ? $ok : $fail)
+	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext) . tag('br');
     }
-    $htm .= (!get_magic_quotes_runtime() ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_magic_quotes'].tag('br')."\n";
-    $htm .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_encoding'].tag('br').tag('br')."\n";
-    $htm .= (file_exists($pth['folder']['plugins'].'jquery/jquery.inc.php') ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_jquery'].tag('br').tag('br')."\n";
+    $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
+	. '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br');
+    $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
+	. '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br') . tag('br');
+    $o .= (file_exists($pth['folder']['plugins'].'jquery/jquery.inc.php') ? $ok : $fail)
+	. '&nbsp;&nbsp;' . $ptx['syscheck_jquery'] . tag('br') . tag('br');
     foreach (array('config/', 'css/', 'languages/') as $folder) {
-	$folders[] = $pth['folder']['plugins'].'roundabout/'.$folder;
+	$folders[] = $pth['folder']['plugins'] . 'roundabout/' . $folder;
     }
     $folders[] = ROUNDABOUT_GALLERY_FOLDER;
     foreach ($folders as $folder) {
-	$htm .= (is_writable($folder) ? $ok : $warn)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_writable'], $folder).tag('br')."\n";
+	$o .= (is_writable($folder) ? $ok : $warn)
+	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder) . tag('br');
     }
-    return $htm;
+    return $o;
 }
 
 
-/**
+/*
  * Handle the plugin administration.
  */
-if (isset($roundabout)) {
+if (isset($roundabout) && $roundabout == 'true') {
     $o .= print_plugin_admin('off');
     switch ($admin) {
-	case '':
-	    $o .= roundabout_version().roundabout_system_check();
-	    break;
-	default:
-	    $o .= plugin_admin_common($action, $admin, $plugin);
+    case '':
+	$o .= Roundabout_version() . Roundabout_systemCheck();
+	break;
+    default:
+	$o .= plugin_admin_common($action, $admin, $plugin);
     }
 }
 
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
 /**
- * Front-End of Roundabout_XH.
+ * Front-end of Roundabout_XH.
  *
- * Copyright (c) 2012 Christoph M. Becker (see license.txt)
+ * @package	Roundabout
+ * @copyright	Copyright (c) 2012-2013 Christoph M. Becker <http://3-magi.net/>
+ * @license	http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version     $Id$
+ * @link	http://3-magi.net/?CMSimple_XH/Roundabout_XH
  */
-
-// utf8-marker: äöüß
 
 
 define('ROUNDABOUT_VERSION', '1beta2');
@@ -21,65 +23,69 @@ define('ROUNDABOUT_FIELD_MIME', 5);
 define('ROUNDABOUT_FIELD_DESC', 6);
 
 
-define('ROUNDABOUT_GALLERY_FOLDER', rtrim($pth['folder']['base'].$plugin_cf['roundabout']['gallery_folder'], '/').'/');
+define('ROUNDABOUT_GALLERY_FOLDER',
+       rtrim($pth['folder']['base'] . $plugin_cf['roundabout']['gallery_folder'],
+	     '/')
+       . '/');
 
 
 /**
  * Returns a list of all photos in $album.
  *
- * @param int $album  The album/gallery ID.
+ * @global array
+ * @global array
+ * @param  int $album  The album/gallery ID.
  * @return array
  */
-function roundabout_photos($album) {
+function Roundabout_photos($album)
+{
     global $pth, $plugin_cf;
     static $recs = NULL;
 
     if (is_null($recs)) {
 	$pcf = $plugin_cf['roundabout'];
-	$fn = ROUNDABOUT_GALLERY_FOLDER.'data/photo.txt';
+	$fn = ROUNDABOUT_GALLERY_FOLDER . 'data/photo.txt';
 	$lines = file($fn, FILE_IGNORE_NEW_LINES);
 	$recs = array();
 	foreach ($lines as $line) {
 	    $recs[] = explode($pcf['gallery_separator'], $line);
 	}
     }
-    return array_values(array_filter($recs, create_function('$elt',
-	    'return $elt['.ROUNDABOUT_FIELD_ALBUM.'] == '.$album.';')));
+    $filter = create_function('$elt',
+			      'return $elt[' . ROUNDABOUT_FIELD_ALBUM . '] == '
+			      . $album . ';');
+    return array_values(array_filter($recs, $filter));
 }
 
 
 /**
- * Creates the JSON file for $album and returns the file name.
+ * Delivers the JSON for $album.
  *
- * @param int $album  The album/gallery ID.
- * @return string
+ * @global array
+ * @global array
+ * @param  int $album  The album/gallery ID.
+ * @return void
  */
-function roundabout_json($album) {
+function Roundabout_json($album) // TODO: use json_encode
+{
     global $pth, $plugin_cf;
 
     $pcf = $plugin_cf['roundabout'];
-    $fn = $pth['folder']['base'].$pcf['gallery_folder'].'images/thumbs/';
-    $o = '{'."\n".'    "items": [';
-    $albums = roundabout_photos($album);
+    $fn = ROUNDABOUT_GALLERY_FOLDER . 'images/thumbs/';
+    $o = '{' . "\n" . '    "items": [';
+    $albums = Roundabout_photos($album);
     foreach ($albums as $i => $photo) {
-	$o .= '{'."\n"
-		."\t".'"id": "'.$photo[ROUNDABOUT_FIELD_ID].'",'."\n"
-		."\t".'"bez": "img'.$photo[ROUNDABOUT_FIELD_ID].'",'."\n"
-		."\t".'"src": "'.$fn.$photo[ROUNDABOUT_FIELD_ID].'t.jpg",'."\n"
-		."\t".'"template": "'.'",'."\n"
-		."\t".'"title": "'.addcslashes($photo[ROUNDABOUT_FIELD_DESC], "\\\"").'",'."\n"
-		."\t".'"description": "'.'"'."\n"
-		.'    }';
+	$o .= '{' . "\n"
+	    . "\t" . '"id": "' . $photo[ROUNDABOUT_FIELD_ID] . '",' . "\n"
+	    . "\t" . '"bez": "img' . $photo[ROUNDABOUT_FIELD_ID] . '",' . "\n"
+	    . "\t" . '"src": "' . $fn . $photo[ROUNDABOUT_FIELD_ID] . 't.jpg",' . "\n"
+	    . "\t" . '"template": "' . '",' . "\n"
+	    . "\t" . '"title": "' . addcslashes($photo[ROUNDABOUT_FIELD_DESC], "\\\"") . '",' . "\n"
+	    . "\t" . '"description": "' . '"' . "\n"
+	    . '    }';
 	if ($i < count($albums) - 1) {$o .= ', ';}
     }
-    $o .= ']'."\n".'}'."\n";
-//    $fn = $pth['folder']['base'].$pcf['gallery_folder'].'data/roundabout_'.$album.'.json';
-//    if (($fh = fopen($fn, 'w')) === FALSE || fwrite($fh, $o) === FALSE) {
-//	e('cntwriteto', 'file', $fn);
-//	$fn = NULL;
-//    }
-//    if ($fh !== FALSE) {fclose($fh);}
-//    return $fn;
+    $o .= ']' . "\n" . '}' . "\n";
     header('Content-Type: text/plain; charset=UTF-8');
     echo $o;
     exit;
@@ -89,25 +95,31 @@ function roundabout_json($album) {
 /**
  * Includes the JS and CSS to the <head>.
  *
- * @param int $album  The album/gallery ID.
- * @global $hjs
+ * @global array
+ * @global string
+ * @global string
+ * @global array
+ * @param  int $album  The album/gallery ID.
  * @return void
  */
-function roundabout_js($album) {
+function Roundabout_js($album)
+{
     global $pth, $hjs, $sn, $plugin_cf;
 
     $pcf = $plugin_cf['roundabout'];
-    include_once $pth['folder']['plugins'].'jquery/jquery.inc.php';
-    include_jquery();
-    $hjs .= tag('link rel="stylesheet" href="'.$pth['folder']['plugins'].'roundabout/css/colorbox.css" type="text/css"');
-    include_jqueryplugin('jCarousel', $pth['folder']['plugins'].'roundabout/lib/carousel-0.9.3.js');
-    include_jqueryplugin('colorbox', $pth['folder']['plugins'].'roundabout/lib/jquery.colorbox-min.js');
+    include_once $pth['folder']['plugins'] . 'jquery/jquery.inc.php';
+    include_jQuery();
+    $hjs .= tag('link rel="stylesheet" href="' . $pth['folder']['plugins']
+		. 'roundabout/css/colorbox.css" type="text/css"');
+    include_jQueryPlugin('jCarousel', $pth['folder']['plugins']
+			 . 'roundabout/lib/carousel-0.9.3.js');
+    include_jQueryPlugin('colorbox', $pth['folder']['plugins']
+			 . 'roundabout/lib/jquery.colorbox-min.js');
     $show_title = $pcf['show_title'] ? 'true' : 'false';
     //$show_buttons = $pcf['show_buttons'] ? 'true' : 'false';
     $json = "$sn?&roundabout_json=$album";
-    $imgdir = ROUNDABOUT_GALLERY_FOLDER.'images/';
+    $imgdir = ROUNDABOUT_GALLERY_FOLDER . 'images/';
     $hjs .= <<<SCRIPT
-
 <script type="text/javascript">
 /* <![CDATA[ */
 jQuery(function() {
@@ -141,30 +153,35 @@ jQuery(function() {
 })
 /* ]]> */
 </script>
-
 SCRIPT;
 }
 
 
 /**
- * Returns the carousel view of $album.
+ * Returns the carousel view of an album.
  *
  * @access public
- * @param int $album  The album/gallery ID.
+ *
+ * @global array
+ * @param  int $album  The album/gallery ID.
  * @return string  The (X)HTML.
  */
-function roundabout($album) {
+function Roundabout($album)
+{
     global $plugin_tx;
 
-    roundabout_js($album);
-    $o = '<div id="roundabout_'.$album.'" class="roundabout"><noscript><div>'
-	    .$plugin_tx['roundabout']['message_noscript'].'</div></noscript></div>';
+    Roundabout_js($album);
+    $o = '<div id="roundabout_' . $album . '" class="roundabout"><noscript><div>'
+	. $plugin_tx['roundabout']['message_noscript'] . '</div></noscript></div>';
     return $o;
 }
 
 
-if (isset($_GET['roundabout_json'])) {
-    roundabout_json($_GET['roundabout_json']);
+/*
+ * Handle the request of JSON data.
+ */
+if (isset($_GET['roundabout_json']) && is_numeric($_GET['roundabout_json'])) {
+    Roundabout_json(intval($_GET['roundabout_json']));
 }
 
 ?>
