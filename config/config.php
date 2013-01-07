5705ad398f49d327eccee5bec3312ecb63496688
cmb69 <cmbecker69@gmx.de>
2013-01-07 12:24:13 +0000

    finished for 1rc1 release

--- config/config.php
+++ config/config.php
@@ -1,21 +1,24 @@
 <?php
 
-	$plugin_cf['roundabout']['gallery_data']="content/plugins/kissgallery/data/";
-	$plugin_cf['roundabout']['gallery_images']="content/plugins/kissgallery/images/";
-	$plugin_cf['roundabout']['gallery_separator']="#";
-	$plugin_cf['roundabout']['size_width']="600";
-	$plugin_cf['roundabout']['size_height']="250";
-	$plugin_cf['roundabout']['speed']="200";
-	$plugin_cf['roundabout']['speed_max']="3";
-	$plugin_cf['roundabout']['perspective']="30";
-	$plugin_cf['roundabout']['dynamic']="60";
-	$plugin_cf['roundabout']['show_title']="1";
-	$plugin_cf['roundabout']['reflection']="0";
-	$plugin_cf['roundabout']['reflection_start']="50";
-	$plugin_cf['roundabout']['reflection_end']="100";
-	$plugin_cf['roundabout']['overlay_perspective']="60";
-	$plugin_cf['roundabout']['overlay_global']="0";
-	$plugin_cf['roundabout']['overlay_color']="#ffffff";
-	$plugin_cf['roundabout']['template_call']="0";
+$plugin_cf['roundabout']['gallery_data']="content/plugins/fotogalerie/data/";
+$plugin_cf['roundabout']['gallery_images']="content/plugins/fotogalerie/images/";
+$plugin_cf['roundabout']['gallery_separator']="§§§";
+
+$plugin_cf['roundabout']['size_width']="600";
+$plugin_cf['roundabout']['size_height']="250";
+$plugin_cf['roundabout']['speed']="200";
+$plugin_cf['roundabout']['speed_max']="3";
+$plugin_cf['roundabout']['perspective']="30";
+$plugin_cf['roundabout']['dynamic']="60";
+
+$plugin_cf['roundabout']['show_title']="1";
+
+$plugin_cf['roundabout']['reflection']="0";
+$plugin_cf['roundabout']['reflection_start']="50";
+$plugin_cf['roundabout']['reflection_end']="100";
+
+$plugin_cf['roundabout']['overlay_perspective']="60";
+$plugin_cf['roundabout']['overlay_global']="0";
+$plugin_cf['roundabout']['overlay_color']="#ffffff";
 
 ?>
